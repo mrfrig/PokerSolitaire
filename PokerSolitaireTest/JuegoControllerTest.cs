3eb3645fdc47beef58510970d9e988a47b77dfa4
Franklin Isalguez <frig.ds@gmail.com>
2013-10-14 00:50:58 -0400

    Agrega esqueleto vacio de pruebas unitarias

---
+++ PokerSolitaireTest/JuegoControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PokerSolitaireTest
+{
+    [TestClass]
+    public class JuegoControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
