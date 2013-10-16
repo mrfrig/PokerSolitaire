4b26253f19f7783a09087e5d7c22faec5d80953f
Franklin Isalguez <frig.ds@gmail.com>
2013-10-16 02:19:15 -0400

    Implementa clase turno y sus test unitarios
    Este commit implementa todos los metodos de la clase turno
    y todos sus test unitarios que garantizan su correcto funcionamiento,
    ademas de esto incluye algunos cambios en el diseno original de la clase,
    tales como:
    - Quitar static a propiedad Cartas

--- PokerSolitaire/Model/Turno.cs
+++ PokerSolitaire/Model/Turno.cs
@@ -5,29 +5,36 @@ using System.Text;
 
 namespace PokerSolitaire.Model
 {
-    /**
-     * Representa un turno del juego, posee un arreglo de 4 cartas, un nombre y una puntuacion segun la combinacion de cartas obtenida, y una puntuacion acumulada hasta ese turno en el juego.
-     */
+    /// <summary>
+    /// Representa un turno del juego, posee un arreglo de 4 cartas, 
+    /// un nombre y una puntuacion segun la combinacion de cartas obtenida, 
+    /// y una puntuacion acumulada hasta ese turno en el juego.
+    /// </summary>
     public class Turno
     {
         private Carta[] cartas;
 
-        private readonly string nombreCombinacion;
+        private string nombreCombinacion;
 
         private int puntuacion;
 
         private int puntuacionAcumulada;
 
+        /// <summary>
+        /// Retorna el nombre de la combinacion obtenida segun las cartas que tiene el turno
+        /// </summary>
         public string NombreCombinacion
         {
             get
             {
                 return nombreCombinacion;
-
             }
 
         }
 
+        /// <summary>
+        /// Retorna la puntuacion obtenida segun la combinacion de cartas del turno
+        /// </summary>
         public int Puntuacion
         {
             get
@@ -38,6 +45,9 @@ namespace PokerSolitaire.Model
 
         }
 
+        /// <summary>
+        /// Retorna la puntuacion acumulada hasta este turno
+        /// </summary>
         public int PuntuacionAcumulada
         {
             get
@@ -48,40 +58,98 @@ namespace PokerSolitaire.Model
 
         }
 
-        public static string[] _cartas;
-
-        public static string[] Cartas
+        /// <summary>
+        /// Retorna un arreglo de cartas representadas como strings
+        /// </summary>
+        public string[] Cartas
         {
             get
             {
-                return _cartas;
-
+                return new string[]{cartas[0].CartaString, cartas[1].CartaString, cartas[2].CartaString, cartas[3].CartaString};
             }
-
         }
 
-        /**
-         * Genera un turno tomando cartas del mazo
-         */
+        /// <summary>
+        /// Genera un turno tomando 4 cartas del mazo en clase Carta
+        /// </summary>
+        /// <param name="puntuacionAcumulada">Puntuacion Acumulada en el turno anterior al turno a registrar</param>
         public Turno(int puntuacionAcumulada)
         {
+            cartas = new Carta[4];
+            for (int i = 0; i < cartas.Length; i++)
+            {
+                cartas[i] = Carta.ObtenerCartaDeMazo();
+            }
 
+            DeterminarCombinacion();
+
+            this.puntuacionAcumulada = puntuacionAcumulada + this.puntuacion;
         }
 
-        /**
-         * Genera un turno creando cartas desde un arreglo de strings y eliminando dichas cartas del mazo
-         */
+        /// <summary>
+        /// Genera un turno creando cartas desde un arreglo de strings y eliminando dichas cartas del mazo
+        /// </summary>
+        /// <param name="cartas">Arreglo de strings que representa las 4 cartas a registrar en turno</param>
+        /// <param name="puntuacionAcumulada">Puntuacion Acumulada en el turno anterior al turno a registrar</param>
         public Turno(string[] cartas, int puntuacionAcumulada)
         {
+            this.cartas = new Carta[cartas.Length];
 
+            for (int i = 0; i < cartas.Length; i++)
+			{
+			    for (int j = 0; j < Carta.VALORES.Length; j++)
+			    {
+                    for (int k = 0; k < Carta.PALOS.Length; k++)
+                    {
+                        if ( (cartas[i]) == (Carta.VALORES[j] + Carta.PALOS[k]) )
+                        {
+                            this.cartas[i] = new Carta(Carta.VALORES[j], Carta.PALOS[k]);
+
+                            Carta.RemoverCartaDeMazo(this.cartas[i]);
+                        }
+                    }
+			    }
+			}
+
+            DeterminarCombinacion();
+
+            this.puntuacionAcumulada = puntuacionAcumulada + this.puntuacion;
         }
 
-        /**
-         * Determina la combinacion obtenida en un arreglo de 4 cartas.
-         */
+        /// <summary>
+        /// Determina la combinacion obtenida del arreglo de cartas del turno
+        /// </summary>
         private void DeterminarCombinacion()
         {
+            if (Carta.DeterminarSiConsecutiva(this.cartas) && Carta.DeterminarSiMismoPalo(this.cartas))
+            {
+                this.puntuacion = 500;
+                this.nombreCombinacion = "Straight Flush";
+            }
 
+            else if (Carta.DeterminarSiConsecutiva(this.cartas))
+            {
+                this.puntuacion = 300;
+                this.nombreCombinacion = "Straight";
+            }
+
+            else if (Carta.DeterminarSiMismoValor(this.cartas))
+            {
+                this.puntuacion = 100;
+                this.nombreCombinacion = "Full House";
+            }
+
+            else if (Carta.DeterminarSiMismoPalo(this.cartas))
+            {
+                this.puntuacion = 10;
+                this.nombreCombinacion = "Flush";
+            }
+
+            else
+            {
+                this.puntuacion = -10;
+                this.nombreCombinacion = "Ninguna";
+            }
         }
     }
 }
--- PokerSolitaireTest/TurnoTest.cs
+++ PokerSolitaireTest/TurnoTest.cs
@@ -1,10 +1,167 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokerSolitaire.Model;
+using System.Collections.Generic;
 
 namespace PokerSolitaireTest
 {
     [TestClass]
     public class TurnoTest
     {
+        Turno turnoRandom;
+        Turno turnoStraightFlush;
+        Turno turnoStraight;
+        Turno turnoFullHouse;
+        Turno turnoFlush;
+        Turno turnoNinguna;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            turnoStraightFlush = new Turno(new string[] { "4C", "5C", "6C", "7C" }, 100);
+            turnoStraight = new Turno(new string[] { "4H", "5D", "6C", "7D" }, -10);
+            turnoFullHouse = new Turno(new string[] { "10H", "10D", "10C", "10D" }, 1000);
+            turnoFlush = new Turno(new string[] { "4H", "5H", "6H", "8H" }, 10);
+            turnoNinguna = new Turno(new string[] { "4H", "5D", "6H", "8H" }, -100);
+        }
+
+        [TestMethod]
+        public void TestTurnoRandomElimina4Cartas()
+        {
+            Carta.GenerarMazoDeCartas();
+
+            turnoRandom = new Turno(400);
+
+            Assert.AreEqual(48, Carta.CartasRestantesEnMazo);
+        }
+
+        [TestMethod]
+        public void TestTurnoRandomCartasRegistradasEnTurnoNoEstanEnMazo()
+        {
+            Carta.GenerarMazoDeCartas();
+
+            turnoRandom = new Turno(400);
+
+            for (int i = 0; i < turnoRandom.Cartas.Length; i++)
+            {
+                Assert.IsFalse(ContieneCarta(Carta.Mazo, turnoRandom.Cartas[i]));
+            }
+        }
+
+        [TestMethod]
+        public void TestTurnoNoRandomPuntuacionDeCombinacionStraightFlush()
+        {
+            Assert.AreEqual(500, turnoStraightFlush.Puntuacion);
+        }
+
+        [TestMethod]
+        public void TestTurnoNoRandomPuntuacionAcumuladaStraightFlush()
+        {
+            Assert.AreEqual(600, turnoStraightFlush.PuntuacionAcumulada);
+        }
+
+        [TestMethod]
+        public void TestTurnoNoRandomNombreDeCombinacionStraightFlush()
+        {
+            Assert.AreEqual("Straight Flush", turnoStraightFlush.NombreCombinacion);
+        }        
+
+        [TestMethod]
+        public void TestTurnoNoRandomPuntuacionDeCombinacionStraight()
+        {
+            Assert.AreEqual(300, turnoStraight.Puntuacion);
+        }
+
+        [TestMethod]
+        public void TestTurnoNoRandomPuntuacionAcumuladaStraight()
+        {
+            Assert.AreEqual(290, turnoStraight.PuntuacionAcumulada);
+        }
+
+        [TestMethod]
+        public void TestTurnoNoRandomNombreDeCombinacionStraight()
+        {
+            Assert.AreEqual("Straight", turnoStraight.NombreCombinacion);
+        }
+
+        [TestMethod]
+        public void TestTurnoNoRandomPuntuacionDeCombinacionFullHouse()
+        {
+            Assert.AreEqual(100, turnoFullHouse.Puntuacion);
+        }
+
+        [TestMethod]
+        public void TestTurnoNoRandomPuntuacionAcumuladaFullHouse()
+        {
+            Assert.AreEqual(1100, turnoFullHouse.PuntuacionAcumulada);
+        }
+
+        [TestMethod]
+        public void TestTurnoNoRandomNombreDeCombinacionFullHouse()
+        {
+            Assert.AreEqual("Full House", turnoFullHouse.NombreCombinacion);
+        }
+
+        [TestMethod]
+        public void TestTurnoNoRandomPuntuacionDeCombinacionFlush()
+        {
+            Assert.AreEqual(10, turnoFlush.Puntuacion);
+        }
+
+        [TestMethod]
+        public void TestTurnoNoRandomPuntuacionAcumuladaFlush()
+        {
+            Assert.AreEqual(20, turnoFlush.PuntuacionAcumulada);
+        }
+
+        [TestMethod]
+        public void TestTurnoNoRandomNombreDeCombinacionFlush()
+        {
+            Assert.AreEqual("Flush", turnoFlush.NombreCombinacion);
+        }
+
+        [TestMethod]
+        public void TestTurnoNoRandomPuntuacionDeCombinacionNinguna()
+        {
+            Assert.AreEqual(-10, turnoNinguna.Puntuacion);
+        }
+
+        [TestMethod]
+        public void TestTurnoNoRandomPuntuacionAcumuladaNinguna()
+        {
+            Assert.AreEqual(-110, turnoNinguna.PuntuacionAcumulada);
+        }
+
+        [TestMethod]
+        public void TestTurnoNoRandomNombreDeCombinacionNinguna()
+        {
+            Assert.AreEqual("Ninguna", turnoNinguna.NombreCombinacion);
+        }
+
+        [TestMethod]
+        public void TestTurnoNoRandomNombreDeCombinacionEliminaCartasDeMazo()
+        {
+            Carta[] cartas = Carta.Mazo;
+
+            for (int i = 0; i < turnoNinguna.Cartas.Length; i++)
+            {
+                Assert.IsFalse(ContieneCarta(cartas, turnoNinguna.Cartas[i]));
+            }
+        }
+
+        private bool ContieneCarta(Carta[] cartas, string carta)
+        {
+            bool contieneCarta = false;
+
+            for (int i = 0; i < cartas.Length; i++)
+            {
+                if (cartas[i].CartaString == carta)
+                {
+                    contieneCarta = true;
+                }
+            }
+
+            return contieneCarta;
+        }
     }
 }
